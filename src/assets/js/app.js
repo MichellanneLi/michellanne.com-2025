6f88995d1880702890c767cdba1abea4819e9dcb
MichellanneLi <michellanne.li@gmail.com>
2022-01-01 16:46:21 -0500

    restrutures header and navigation to use foundation top bar and title bar features.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,3 +15,15 @@ require('foundation-sites');
 
 
 $(document).foundation();
+
+////////////////////////////////////////
+// Mobile Nav Trigger
+////////////////////////////////////////
+$('.menu-icon').click(function() {
+  if ($(this).hasClass('is-active')) {
+    $(this).removeClass('is-active');
+  } else {
+    // If clicked item not active, add classes
+    $(this).addClass('is-active');
+  }
+});
